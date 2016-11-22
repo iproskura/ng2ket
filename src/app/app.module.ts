c1b4f03b71d811c772267b06aa5ff6b3646a5ab9
Igor Proskura <is.proskura@gmail.com>
2016-11-22 14:07:42 +0200

    stage0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { Services1Component } from './services1/services1.component';
-import { Services2Component } from './services2/services2.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { StatisticsComponent } from './statistics/statistics.component';
-import { AboutComponent } from './about/about.component';
-import { NewsComponent } from './news/news.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { FooterComponent } from './footer/footer.component';
-import { TeamComponent } from './team/team.component';
+import { AboutComponent } from "./about/about.component";
+import { AppComponent } from "./app.component";
+import { ContactsComponent } from "./contacts/contacts.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HeaderComponent } from "./header/header.component";
+import { NewsComponent } from "./news/news.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { Services1Component } from "./services1/services1.component";
+import { Services2Component } from "./services2/services2.component";
+import { StatisticsComponent } from "./statistics/statistics.component";
+import { TeamComponent } from "./team/team.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
+    ContactsComponent,
+    FooterComponent,
     HeaderComponent,
+    NewsComponent,
     Services1Component,
     Services2Component,
     PortfolioComponent,
     StatisticsComponent,
-    AboutComponent,
-    NewsComponent,
-    ContactsComponent,
-    FooterComponent,
     TeamComponent
   ],
   imports: [
@@ -37,4 +37,5 @@ import { TeamComponent } from './team/team.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

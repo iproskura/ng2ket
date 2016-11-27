471be62cc9afc69002b2caa855000572062a500a
Igor Proskura <khors.fb@gmail.com>
2016-11-27 23:28:37 +0200

    added services1

--- src/app/services1/services1.component.ts
+++ src/app/services1/services1.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services1.component.css']
 })
 export class Services1Component implements OnInit {
+  messageId: number;
 
-  constructor() { }
+  services: Service[] = [{
+    id: 1,
+    name: 'web',
+    src: 'service_00.png'
+  }, {
+    id: 2,
+    name: 'design',
+    src: 'service_01.png'
+  }, {
+    id: 3,
+    name: 'programming',
+    src: 'service_02.png'
+  }, {
+    id: 4,
+    name: 'photography',
+    src: 'service_03.png'
+  }
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  showMessage(id){
+    this.messageId = id;
+  }
+}
+
+class Service {
+  id: number;
+  name: string;
+  src: string
 }
